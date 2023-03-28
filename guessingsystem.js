9e672680cb1434b3a862e8e0339491a8826aa03c
Teknoses <99210035+Teknoses@users.noreply.github.com>
2023-03-28 14:51:21 +0000

    Added guessing system

---
+++ guessingsystem.js
@@ -0,0 +1,26 @@
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  class puzzle{
+  constructor(phrase,hint,category,difficulty){
+    this.phrase = phrase
+    this.hint = hint
+    this.category = category
+    this.difficulty = difficulty
+  }
+}
+  const puzzle = [
+     new puzzle('hi','greeting','words','easy'),
+    new puzzle('computer','box','technology','medium')
+  ]  
+  
+}
+
+function draw() {
+  clear();
+}
+
