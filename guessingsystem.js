32a5be20a2c4f27fd488b300eb63e83b23006105
Teknoses <99210035+Teknoses@users.noreply.github.com>
2023-03-29 14:51:35 +0000

    Finished Guessing system

--- guessingsystem.js
+++ guessingsystem.js
@@ -1,10 +1,4 @@
-
-
-
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+//Defines what a puzzle is
   class puzzle{
   constructor(phrase,hint,category,difficulty){
     this.phrase = phrase
@@ -12,15 +6,53 @@ function setup() {
     this.category = category
     this.difficulty = difficulty
   }
-}
-  const puzzle = [
-     new puzzle('hi','greeting','words','easy'),
-    new puzzle('computer','box','technology','medium')
+}  
+//Creates a bunch of puzzles
+const allPuzzles = [
+    new puzzle('computer screen','LEDS','technology','medium'),
+		new puzzle('snake','long animal','animial','easy'),
   ]  
-  
+  let currentpuzzle
+  let guesses
+	let guessPositionX = 100
+	let guessPositionY = 100
+	let guessLetterGap = 20	
+	let currentphrase
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(100);
+  currentpuzzle = random(allPuzzles)
+  guesses = []
+	currentphrase = currentpuzzle.phrase
+  for (let i = 0; i < currentphrase.length; i++) {
+		letter = currentphrase[i]
+		if (letter == ' ') guesses.push(letter)
+		else guesses.push('_')
+	}
 }
 
 function draw() {
-  clear();
+	clear()
+	drawPuzzle()
 }
 
+function drawPuzzle() {
+	for (let i = 0; i < guesses.length; i++) {
+		letter = guesses[i]
+		text(letter, guessPositionX + i * guessLetterGap, guessPositionY)
+	}
+}
+
+function keyPressed() {
+	let correctCount = 0
+	for (let i = 0; i < currentphrase.length; i++) {
+		letter = currentphrase[i]
+		if (key == letter) {
+			correctCount++
+			guesses[i] = letter
+		}
+	}
+	if (correctCount == 0) {
+		print(`Wrong! there is no "${key}"`)
+	}
+}
