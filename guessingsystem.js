ef5b759c95860f53d74e75174e624bdcab807063
Ikyuuko <>
2023-04-05 14:33:37 +0000

    Added points system and fixed the infinite added points when clicking the right key over and over. Added lives system and image. Also added a game over menu(WIP)

--- guessingsystem.js
+++ guessingsystem.js
@@ -1,3 +1,8 @@
+let heart
+function preload(){
+  heart = loadImage('heart.png')
+}
+
 //Defines what a puzzle is
   class puzzle{
   constructor(phrase,hint,category,difficulty){
@@ -13,13 +18,24 @@
   new puzzle('computer screen','LEDS','technology','medium'),
 	new puzzle('snake','long animal','animial','easy'),
   ]  
+  let currentHint
   let currentpuzzle
   let guesses
-	let guessPositionX = 100
-	let guessPositionY = 100
-	let guessLetterGap = 20	
+	let guessPositionX = 500
+	let guessPositionY = 400
+	let guessLetterGap = 40	
 	let currentphrase
   let switchCurrentPuzzle = true;
+
+//point system
+let points = 0
+
+//player lives counter
+let lives = 3
+
+//menuscreens
+let gameover = false
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(100);
@@ -27,17 +43,40 @@ function setup() {
 }
 
 function draw() {
+  gameoverscreen()
+  if (gameover == false) {
 	clear()
+  drawBackground()
+    
   if(switchCurrentPuzzle == true){
      switchPuzzle()
   }
-	drawPuzzle()
+	
+  drawPuzzle()
+  drawHint()
+  push()
+  textSize(50)
+  text("game points: " + points, 100, 50)
+  text("lives: ", 1200, 50)
+  pop()
+  drawLives()
+  }
+}
+
+function drawBackground() {
+  push()
+  fill('gray')
+  rect(0, 0, windowWidth, windowHeight)
+  pop()
 }
 
 function drawPuzzle() {
 	for (let i = 0; i < guesses.length; i++) {
 		letter = guesses[i]
+    push()
+    textSize(50)
 		text(letter, guessPositionX + i * guessLetterGap, guessPositionY)
+    pop()
 	}
 }
 
@@ -52,22 +91,68 @@ function keyPressed() {
           print(`You already guessed "${key}"`)
           break
         }
+      points = points + 50
 			guesses[i] = letter
 		}
+  }
+	if (correctCount == 0) { 
+   
+    if (lives > 0) {
+      lives = lives - 1
+    }
+    if(lives == 0){
+      gameover = true
+       print("out of lives")
+    }
+   else{ 
+     print(`Wrong! there is no "${key}"`)
+   }
 	}
-	if (correctCount == 0) {
-		print(`Wrong! there is no "${key}"`)
-	}
+
 }
 
 function switchPuzzle(){
     currentpuzzle = random(allPuzzles)
   guesses = []
 	currentphrase = currentpuzzle.phrase
+  currentHint = currentpuzzle.hint
   for (let i = 0; i < currentphrase.length; i++) {
 		letter = currentphrase[i]
 		if (letter == ' ') guesses.push(letter)
 		else guesses.push('_')
 	}
   switchCurrentPuzzle = false;
+}
+
+function gameoverscreen() {
+  if (gameover == true)
+    fill('red')
+    rect(0, 0, windowWidth, windowHeight)
+    text('Game Over!', 150, 125)
+    text('Out of Lives </3', 150, 175)
+}
+
+function drawHeart (posx, posy, length, height) {
+
+   image(heart, posx, posy, length, height)
+
+}
+
+function drawLives() {
+  if (lives == 3) {
+    drawHeart(1500, 40, 20, 20)
+
+  }
+  if (lives >= 2) {
+    drawHeart(1550, 40, 20, 20)
+  }
+  if (lives >=1) {
+    drawHeart(1600, 40, 20, 20)
+  }
+}
+
+function drawHint(){
+  if(lives <= 2){
+    text(currentHint, 200, 200)
+  }
 }
